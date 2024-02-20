d72f8a570d91d15126a5e7613cb1ddec1fbcaeef
meldawanimelda <trimeldawanim@gmail.com>
2024-02-20 12:21:21 +0700

    tugas php

---
+++ tugas/1b.php
@@ -0,0 +1,16 @@
+<?php
+$npm_last_two_digits = 15;
+
+$result = $npm_last_two_digits * 5;
+echo "Aku adalah angka $npm_last_two_digits<br>";
+echo "Jika aku dikali 5, maka aku sekarang menjadi $result<br>";
+
+$result /= 2;
+echo "Jika aku dibagi 2, maka aku sekarang menjadi $result<br>";
+
+$result += 75;
+echo "Jika aku ditambah 75, maka aku sekarang menjadi $result<br>";
+
+$result -= 20;
+echo "Jika aku dikurang 20, maka aku sekarang menjadi $result<br>";
+?>
